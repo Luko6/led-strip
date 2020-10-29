354ff4481c472f01edc1edf6b2c57256309f9175
luko6 <luko.luko5235@gmail.com>
2020-10-29 14:14:25 +0100

    added csharp code

---
+++ csharp/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace led_controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Red = new System.Windows.Forms.Button();
+            this.btn_Green = new System.Windows.Forms.Button();
+            this.btn_Blue = new System.Windows.Forms.Button();
+            this.btn_Yellow = new System.Windows.Forms.Button();
+            this.btn_Cyan = new System.Windows.Forms.Button();
+            this.btn_Purple = new System.Windows.Forms.Button();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.btn_White = new System.Windows.Forms.Button();
+            this.btn_Off = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Red
+            // 
+            this.btn_Red.BackColor = System.Drawing.Color.Red;
+            this.btn_Red.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Red.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Red.ForeColor = System.Drawing.Color.White;
+            this.btn_Red.Location = new System.Drawing.Point(12, 12);
+            this.btn_Red.Name = "btn_Red";
+            this.btn_Red.Size = new System.Drawing.Size(75, 75);
+            this.btn_Red.TabIndex = 2;
+            this.btn_Red.Text = "RED";
+            this.btn_Red.UseVisualStyleBackColor = false;
+            this.btn_Red.Click += new System.EventHandler(this.btn_Red_Click);
+            // 
+            // btn_Green
+            // 
+            this.btn_Green.BackColor = System.Drawing.Color.Green;
+            this.btn_Green.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Green.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Green.ForeColor = System.Drawing.Color.White;
+            this.btn_Green.Location = new System.Drawing.Point(93, 12);
+            this.btn_Green.Name = "btn_Green";
+            this.btn_Green.Size = new System.Drawing.Size(75, 75);
+            this.btn_Green.TabIndex = 3;
+            this.btn_Green.Text = "GREEN";
+            this.btn_Green.UseVisualStyleBackColor = false;
+            this.btn_Green.Click += new System.EventHandler(this.btn_Green_Click);
+            // 
+            // btn_Blue
+            // 
+            this.btn_Blue.BackColor = System.Drawing.Color.Blue;
+            this.btn_Blue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Blue.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Blue.ForeColor = System.Drawing.Color.White;
+            this.btn_Blue.Location = new System.Drawing.Point(174, 12);
+            this.btn_Blue.Name = "btn_Blue";
+            this.btn_Blue.Size = new System.Drawing.Size(75, 75);
+            this.btn_Blue.TabIndex = 4;
+            this.btn_Blue.Text = "BLUE";
+            this.btn_Blue.UseVisualStyleBackColor = false;
+            this.btn_Blue.Click += new System.EventHandler(this.btn_Blue_Click);
+            // 
+            // btn_Yellow
+            // 
+            this.btn_Yellow.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Yellow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Yellow.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Yellow.ForeColor = System.Drawing.Color.Black;
+            this.btn_Yellow.Location = new System.Drawing.Point(174, 93);
+            this.btn_Yellow.Name = "btn_Yellow";
+            this.btn_Yellow.Size = new System.Drawing.Size(75, 75);
+            this.btn_Yellow.TabIndex = 5;
+            this.btn_Yellow.Text = "YELLOW";
+            this.btn_Yellow.UseVisualStyleBackColor = false;
+            this.btn_Yellow.Click += new System.EventHandler(this.btn_Yellow_Click);
+            // 
+            // btn_Cyan
+            // 
+            this.btn_Cyan.BackColor = System.Drawing.Color.Cyan;
+            this.btn_Cyan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Cyan.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cyan.ForeColor = System.Drawing.Color.Black;
+            this.btn_Cyan.Location = new System.Drawing.Point(12, 93);
+            this.btn_Cyan.Name = "btn_Cyan";
+            this.btn_Cyan.Size = new System.Drawing.Size(75, 75);
+            this.btn_Cyan.TabIndex = 6;
+            this.btn_Cyan.Text = "CYAN";
+            this.btn_Cyan.UseVisualStyleBackColor = false;
+            this.btn_Cyan.Click += new System.EventHandler(this.btn_Cyan_Click);
+            // 
+            // btn_Purple
+            // 
+            this.btn_Purple.BackColor = System.Drawing.Color.Purple;
+            this.btn_Purple.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Purple.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Purple.ForeColor = System.Drawing.Color.White;
+            this.btn_Purple.Location = new System.Drawing.Point(93, 93);
+            this.btn_Purple.Name = "btn_Purple";
+            this.btn_Purple.Size = new System.Drawing.Size(75, 75);
+            this.btn_Purple.TabIndex = 7;
+            this.btn_Purple.Text = "PURPLE";
+            this.btn_Purple.UseVisualStyleBackColor = false;
+            this.btn_Purple.Click += new System.EventHandler(this.btn_Purple_Click);
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Open.Location = new System.Drawing.Point(12, 174);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(237, 43);
+            this.btn_Open.TabIndex = 8;
+            this.btn_Open.Text = "Open Serial";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Close.Location = new System.Drawing.Point(12, 223);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(237, 43);
+            this.btn_Close.TabIndex = 9;
+            this.btn_Close.Text = "Close Serial";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // btn_White
+            // 
+            this.btn_White.BackColor = System.Drawing.Color.White;
+            this.btn_White.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_White.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_White.ForeColor = System.Drawing.Color.Black;
+            this.btn_White.Location = new System.Drawing.Point(255, 12);
+            this.btn_White.Name = "btn_White";
+            this.btn_White.Size = new System.Drawing.Size(75, 124);
+            this.btn_White.TabIndex = 10;
+            this.btn_White.Text = "WHITE";
+            this.btn_White.UseVisualStyleBackColor = false;
+            this.btn_White.Click += new System.EventHandler(this.btn_White_Click_1);
+            // 
+            // btn_Off
+            // 
+            this.btn_Off.BackColor = System.Drawing.Color.Black;
+            this.btn_Off.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Off.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Off.ForeColor = System.Drawing.Color.White;
+            this.btn_Off.Location = new System.Drawing.Point(255, 142);
+            this.btn_Off.Name = "btn_Off";
+            this.btn_Off.Size = new System.Drawing.Size(75, 124);
+            this.btn_Off.TabIndex = 11;
+            this.btn_Off.Text = "OFF";
+            this.btn_Off.UseVisualStyleBackColor = false;
+            this.btn_Off.Click += new System.EventHandler(this.btn_Off_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(338, 277);
+            this.Controls.Add(this.btn_Off);
+            this.Controls.Add(this.btn_White);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Open);
+            this.Controls.Add(this.btn_Purple);
+            this.Controls.Add(this.btn_Cyan);
+            this.Controls.Add(this.btn_Yellow);
+            this.Controls.Add(this.btn_Blue);
+            this.Controls.Add(this.btn_Green);
+            this.Controls.Add(this.btn_Red);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Red;
+        private System.Windows.Forms.Button btn_Green;
+        private System.Windows.Forms.Button btn_Blue;
+        private System.Windows.Forms.Button btn_Yellow;
+        private System.Windows.Forms.Button btn_Cyan;
+        private System.Windows.Forms.Button btn_Purple;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_White;
+        private System.Windows.Forms.Button btn_Off;
+    }
+}
+
---
+++ csharp/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace led_controller
+{
+    public partial class Form1 : Form
+    {
+        //object serialport to listen usb
+        System.IO.Ports.SerialPort Port;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //configuration of arduino, you check if com3 is the port correct, 
+            //in arduino ide you can make it
+            Port = new System.IO.Ports.SerialPort();
+            Port.PortName = "COM3";
+            Port.BaudRate = 9600;
+            Port.ReadTimeout = 500;
+
+            try
+            {
+                Port.Open();
+                Port.Write("FFFFFF");
+                Port.Write("000000");
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Port.Write("000000");
+            //when the form will be closed this line close the serial port
+            if (Port.IsOpen)
+                Port.Close();
+        }
+
+        private void btn_Red_Click(object sender, EventArgs e)
+        {
+            Port.Write("FF0000");
+        }
+
+        private void btn_Green_Click(object sender, EventArgs e)
+        {
+            Port.Write("00FF00");
+        }
+
+        private void btn_Blue_Click(object sender, EventArgs e)
+        {
+            Port.Write("0000FF");
+        }
+
+        private void btn_Purple_Click(object sender, EventArgs e)
+        {
+            Port.Write("FF00FF");
+        }
+
+        private void btn_Yellow_Click(object sender, EventArgs e)
+        {
+            Port.Write("FFFF00");
+        }
+
+        private void btn_Cyan_Click(object sender, EventArgs e)
+        {
+            Port.Write("00FFFF");
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            if (!Port.IsOpen)
+                Port.Open();
+            Port.Write("FFFFFF");
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            Port.Write("000000");
+            if (Port.IsOpen)
+                Port.Close();
+        }
+
+        private void btn_White_Click_1(object sender, EventArgs e)
+        {
+            Port.Write("FFFFFF");
+        }
+
+        private void btn_Off_Click_1(object sender, EventArgs e)
+        {
+            Port.Write("000000");
+        }
+    }
+}
